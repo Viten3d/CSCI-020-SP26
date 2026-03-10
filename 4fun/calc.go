0001cc67a853a0416796fce81517c09bba92ffbe
Viten3d <90654400+Viten3d@users.noreply.github.com>
2026-03-10 17:19:03 +0000

    side stuf

---
+++ 4fun/calc.go
@@ -0,0 +1,92 @@
+/*
+Calculator
+10 Mar 2026
+*/
+
+package main
+
+import "fmt"
+
+type Expression struct {
+	FirstTerm float64
+	SecondTerm float64
+	Operator string
+}
+
+func (e *Expression) SetFirstTerm(ft float64) {
+	e.FirstTerm = ft
+}
+
+func (e Expression) GetFirstTerm() float64 {
+	return e.FirstTerm
+}
+
+func (e *Expression) SetSecondTerm(st float64) {
+	e.SecondTerm = st
+}
+
+func (e Expression) GetSecondTerm() float64 {
+	return e.SecondTerm
+}
+
+func (e *Expression) SetOperator(op string) {
+	e.Operator = op
+}
+
+func (e Expression) GetOperator() string {
+	return e.Operator
+}
+
+func (e Expression) EvaluateExpression() float64 {
+	if e.Operator == "+" {
+		return e.FirstTerm + e.SecondTerm
+	} else if e.Operator == "-" {
+		return e.FirstTerm - e.SecondTerm
+	} else if e.Operator == "*" {
+		return e.FirstTerm * e.SecondTerm
+	} else { // expecting "/"
+		return e.FirstTerm / e.SecondTerm
+	}
+}
+
+func NewExpression(ft float64, st float64, op string) Expression {
+	var e Expression
+	e.FirstTerm = ft
+	e.SecondTerm = st
+	e.Operator = op
+	return e
+}
+
+func PromptFirstTerm() float64 {
+	var ft float64
+	fmt.Println("Enter the first term:")
+	fmt.Scanln(&ft)
+	return ft
+}
+
+func PromptSecondTerm() float64 {
+	var st float64
+	fmt.Println("Enter the second term:")
+	fmt.Scanln(&st)
+	return st
+}
+
+func PromptOperator() string {
+	var op string
+	fmt.Println("Enter the operator:")
+	fmt.Scanln(&op)
+	return op
+}
+
+func main() {
+	var currentExp Expression = NewExpression(0, 0, "+")
+
+	fmt.Print("Welcome to the calculator program!\n\n")
+
+	currentExp.SetFirstTerm(PromptFirstTerm())
+	currentExp.SetSecondTerm(PromptSecondTerm())
+	currentExp.SetOperator(PromptOperator())
+
+	fmt.Print(currentExp.GetFirstTerm(), currentExp.GetOperator(), currentExp.GetSecondTerm(), " = ")
+	fmt.Println(currentExp.EvaluateExpression())
+}
